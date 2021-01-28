50ca2f6bf5449a1d9553fe28e8750d503667909f
Tom Bredemeier <tbredemeier@barrington220.org>
2021-01-28 09:08:25 -0600

    Initial Commit

---
+++ To Do List/To_Do_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  To_Do_ListApp.swift
+//  To Do List
+//
+//  Created by Tom Bredemeier on 1/28/21.
+//
+
+import SwiftUI
+
+@main
+struct To_Do_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
