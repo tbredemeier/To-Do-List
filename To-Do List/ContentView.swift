29075550dff9b06b4bc99cc01fa582c26fc4263c
Tom Bredemeier <tbredemeier@barrington220.org>
2024-01-23 10:16:32 -0600

    Initial Commit

---
+++ To-Do List/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  To-Do List
+//
+//  Created by Tom Bredemeier on 1/23/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ To-Do List/To_Do_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  To_Do_ListApp.swift
+//  To-Do List
+//
+//  Created by Tom Bredemeier on 1/23/24.
+//
+
+import SwiftUI
+
+@main
+struct To_Do_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
