c92c43428cdf67c82b6d5c1b8b41361ad5020274
Tom Bredemeier <tbredemeier@barrington220.org>
2024-01-23 11:30:05 -0600

    Created Add Items view

---
+++ To-Do List/AddItemView.swift
@@ -0,0 +1,18 @@
+//
+//  AddItemView.swift
+//  To-Do List
+//
+//  Created by Tom Bredemeier on 1/23/24.
+//
+
+import SwiftUI
+
+struct AddItemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddItemView()
+}
